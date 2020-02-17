3aa655c51743f6ad6a149bbd5d00437877f006cd
MBosha <boshyn@gmail.com>
2020-02-17 15:51:27 +0300

    2020-17-02
    15-51

--- mySite/headPage/views.py
+++ mySite/headPage/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.template.loader import render_to_string
 from django.db.models.query_utils import DeferredAttribute
 from django.views.generic.list import ListView
+from django.http import Http404
 
 from .models import Section, Article
 
@@ -20,8 +21,12 @@ class MyView(View):
         #return HttpResponse(body)        
         randomNum1 = random.randint(0, 30)
         randomNum2 = random.randint(0, 100) 
-        randomNum3 = random.randint(0, 100)        
-        articles = self.model.objects.all()
+        randomNum3 = random.randint(0, 100)  
+        try:
+            articles = self.model.objects.all()
+        except Exception:
+            raise Http404("ЭТО ЧТО ТО ЗА НАДПИСЬ")
+            #raise RuntimeError('RuntimeError')
         rend = self.model.objects.count()
         context = {"latest_firstPage": randomNum2, "var_1" : randomNum3, "range": range(randomNum1), "rend" : rend, 'articles':articles}
         return render(request, self.template_name, context=context)
@@ -46,33 +51,11 @@ class MyIndexView(View):
     context_object_name = 'sections'
  
     def get(self, request, *args, **kwargs):
-        """
-        the_data={}
-        the_data1={}        
-        all_lessons= self.model.objects.all()
-        for lesson in all_lessons:
-            the_data1[lesson]=all_lessons.all()
-            for section in the_data1[lesson]:
-                the_data[lesson][section]=section
-                context={'the_data':the_data,}
-        """
+
         sections = self.model.objects.all()
         count = self.model.objects.count()
-        context = {'sections':sections,'count':count,}
-        
-        #context += str(self.model.section_title)
-        #context += self.model.section_url
-        #context += self.model.section_description
-        #context = {Section.section_description, Section.section_title, Section.section_url}
-        #context['section_list'] = Section.Meta.db_table.__getitem__#.Meta.db_table.objects.all().order_by('section_title')
-        #body = 'Hello, Index!<br>'
-        #body1 = context[0]
-        #body2 = body1.blank
-        #body += body2
-        #body = render_to_string('index.html', {'section_list' : context})
- 
+        context = {'sections':sections,'count':count,} 
         return render(request, self.template_name, context=context)
-        #return HttpResponse(context)
  
 class MySectionView(View):
     template_name = 'headPage/section.html'
