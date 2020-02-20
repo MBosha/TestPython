81ad53c4774633126d6d03d7913b80faea1d06f5
MBosha <boshyn@gmail.com>
2020-02-20 15:49:29 +0300

    2020-02-20
    15-49

--- mySite/mySite/settings.py
+++ mySite/mySite/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap3',
     'headPage',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,4 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#empty
